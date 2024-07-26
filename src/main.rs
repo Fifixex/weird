02e5f615b33ff66093b969cbf724fb4ec79feb85
Fifix <122852085+Fifixex@users.noreply.github.com>
2024-07-25 22:54:50 -0300

    ♻️ refactor: CLI commands basic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use crossterm::event::EventStream;
+use app::Application;
+use args::Args;
+
+fn main() -> Result<()> {
+    let mut args = Args::parse_args().context("Could not parse arguments!")?;
+    let help_message = format!("Weird - v{}", env!("CARGO_PKG_VERSION"));
+
+    if args.get_information {
+        print!("{}", help_message);
+        std::process::exit(0);
+    }
+
+    let mut app = Application::new(args).context("Unable to create new application")?;
+    let exit_code = app.run(&mut EventStream::new())?;
+    std::process::exit(exit_code);
 }
