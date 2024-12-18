36c203cc50cedfe5ed0e8f1076eabd5b5bf94269
Fifixex <122852085+Fifixex@users.noreply.github.com>
2024-12-18 19:35:16 +0000

    :sparkles: feat: add `open_file` method to Editor struct

--- src/application.rs
+++ src/application.rs
@@ -1,6 +1,7 @@
-use clap::ArgMatches;
 use crate::editor::Editor;
 use anyhow::Result;
+use clap::ArgMatches;
+use std::path::PathBuf;
 
 pub struct Application {
     editor: Editor,
@@ -9,10 +10,17 @@ pub struct Application {
 impl Application {
     pub fn new(args: ArgMatches) -> Result<Self> {
         let mut editor = Editor::new()?;
-        let app = Self { editor };
-        Ok(app)
+
+        if let Some(files) = args.get_many::<PathBuf>("FILE") {
+            for file in files {
+                editor.open_file(file.clone())?;
+            }
+        }
+
+        Ok(Self { editor })
     }
     pub fn run(&mut self) -> Result<()> {
+        self.editor.render();
         Ok(())
     }
 }
--- src/editor.rs
+++ src/editor.rs
@@ -1,17 +1,45 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
+use std::{fs, path::PathBuf};
+
+pub struct Document {
+    pub path: Option<PathBuf>,
+    pub content: Vec<String>,
+}
+
+impl Document {
+    pub fn open(path: PathBuf) -> Result<Self> {
+        let document = fs::read_to_string(&path)
+            .with_context(|| format!("Failed to open file: {}", path.display()))?;
+        let content = document.lines().map(String::from).collect();
+
+        Ok(Self {
+            path: Some(path),
+            content,
+        })
+    }
+}
 
 pub struct Editor {
     /// Storage of all the documents opened in the editor
     pub documents: Vec<Document>,
-    /// Interface for writing to the terminal
-    pub terminal: Terminal,
 }
 
 impl Editor {
     pub fn new() -> Result<Self> {
-        Ok(Self {
-            documents: vec![],
-            terminal: Terminal::new(),
-        })
+        Ok(Self { documents: vec![] })
+    }
+
+    pub fn open_file(&mut self, path: PathBuf) -> Result<()> {
+        let document = Document::open(path)?;
+        self.documents.push(document);
+        Ok(())
+    }
+
+    pub fn render(&self) {
+        for (i, document) in self.documents.iter().enumerate() {
+            for line in &document.content {
+                println!("{} - {}", i + 1, line)
+            }
+        }
     }
 }
